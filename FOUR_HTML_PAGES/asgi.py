476e6b650e635acd21b15c0e987ed07eab1adbe1
akhila <yarasuakhila19@gmail.com>
2023-11-17 20:03:49 +0530

    first commit

---
+++ FOUR_HTML_PAGES/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for FOUR_HTML_PAGES project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FOUR_HTML_PAGES.settings')
+
+application = get_asgi_application()
